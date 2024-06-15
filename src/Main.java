d7c83f717541f7048f702b95c2e5edec169cbf8a
tayloh2 <tayloh2@mail.uc.edu>
2024-06-15 14:40:30 -0400

    TheaterKiosk.iml

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int customerAge = in.nextInt();
+
+        if (customerAge >= 21)
+        {
+            System.out.println("Please take a wristband");
+
+    }
+}}
